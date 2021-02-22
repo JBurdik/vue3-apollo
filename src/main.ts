e7b7f07ce9496522eda365fcf226354975de0edc
JBurdik <burdik6@gmail.com>
2021-02-22 20:33:18 +0100

    apollo config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,39 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp, h, provide } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+
+const defaultClient = new ApolloClient({
+  uri: "http://graphql.test/graphql",
+  cache: new InMemoryCache()
+});
+
+// const query = gql`
+//   query {
+//     burgers {
+//       name
+//       price
+//       id
+//     }
+//   }
+// `;
+// defaultClient
+//   .query({
+//     query
+//   })
+//   .then((res) => console.log(res));
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, defaultClient);
+  },
+  render() {
+    return h(App);
+  }
+})
+  .use(router)
+  .use(store)
+  .mount("#app");
